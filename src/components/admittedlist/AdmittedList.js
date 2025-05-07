df5043d0e80203f81d4a51f6c324fc936940719e
Sgu-Justin <justin.sye@student.sgu.sc.id>
2025-05-07 16:49:56 +0700

    Fixed styling name to be more clear

--- src/components/admittedlist/AdmittedList.js
+++ src/components/admittedlist/AdmittedList.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import './AdmittedList.css';
 
-// Mock data for admitted patients
 const admittedPatients = [
   { id: '101', name: 'Sarah Johnson', triage: 2, waitTime: '2h 15m', injury: 'Fractured arm' },
   { id: '102', name: 'Michael Chen', triage: 1, waitTime: '1h 45m', injury: 'Head trauma' },
   { id: '103', name: 'Emily Rodriguez', triage: 3, waitTime: '3h 30m', injury: 'Sprained ankle' },
   { id: '104', name: 'David Kim', triage: 4, waitTime: '45m', injury: 'Minor burns' },
   { id: '105', name: 'Jessica Wong', triage: 2, waitTime: '2h 05m', injury: 'Broken wrist' },
-  { id: '106', name: 'Robert Smith', triage: 1, waitTime: '1h 20m', injury: 'Chest pain' },
-  { id: '107', name: 'Amanda Lee', triage: 3, waitTime: '2h 50m', injury: 'Deep cut' },
-  { id: '108', name: 'Daniel Brown', triage: 5, waitTime: '30m', injury: 'Allergic reaction' },
-  { id: '109', name: 'Olivia Davis', triage: 2, waitTime: '2h 25m', injury: 'Concussion' },
-  { id: '110', name: 'James Wilson', triage: 4, waitTime: '50m', injury: 'Eye injury' },
-  { id: '111', name: 'Sophia Martinez', triage: 1, waitTime: '1h 15m', injury: 'Severe bleeding' },
-  { id: '112', name: 'William Taylor', triage: 3, waitTime: '3h 10m', injury: 'Dislocated shoulder' },
-  { id: '113', name: 'Emma Anderson', triage: 2, waitTime: '2h 00m', injury: 'Broken leg' },
-  { id: '114', name: 'Benjamin Thomas', triage: 5, waitTime: '25m', injury: 'Mild fever' },
-  { id: '115', name: 'Ava Jackson', triage: 1, waitTime: '1h 30m', injury: 'Stroke symptoms' },
 ];
 
 const getTriageColor = (level) => {
@@ -27,45 +16,45 @@ const getTriageColor = (level) => {
 
 const AdmittedList = () => {
   return (
-    <div className="admitted-list-container">
-      <div className="admitted-list">
-        {/* Box 1: Title Header */}
-        <div className="list-header">
+    <div className="al-container">
+      <div className="al-list">
+        {/* Title Header */}
+        <div className="al-header">
           <h2>Admitted Patient History</h2>
-          <div className="header-buttons">
-            <button className="download-button">
-              <span className="download-icon">↓</span> Download
+          <div className="al-header-btns">
+            <button className="al-download-btn">
+              <span className="al-download-icon">↓</span> Download
             </button>
-            <button className="clear-button">Clear History</button>
+            <button className="al-clear-btn">Clear History</button>
           </div>
         </div>
 
-        {/* Box 2: Column Headers */}
-        <div className="column-header">
-          <div>P.ID</div>
-          <div>Patient Name</div>
-          <div>Triage</div>
-          <div>Total Wait Time</div>
-          <div>Injury Type</div>
+        {/* Column Headers */}
+        <div className="al-col-header">
+          <div className="al-col-pid">P.ID</div>
+          <div className="al-col-name">Patient Name</div>
+          <div className="al-col-triage">Triage</div>
+          <div className="al-col-time">Total Wait Time</div>
+          <div className="al-col-injury">Injury Type</div>
         </div>
 
-        {/* Box 3: Scrollable Rows */}
-        <div className="patient-rows-container">
-          <div className="patient-rows-scrollable">
+        {/* Patient Rows */}
+        <div className="al-rows-container">
+          <div className="al-rows-scrollable">
             {admittedPatients.map((p) => (
-              <div className="patient-row" key={p.id}>
-                <div>{p.id}</div>
-                <div>{p.name}</div>
-                <div>
+              <div className="al-row" key={p.id}>
+                <div className="al-col-pid">{p.id}</div>
+                <div className="al-col-name">{p.name}</div>
+                <div className="al-col-triage">
                   <span
-                    className="triage-circle"
+                    className="al-triage-circle"
                     style={{ backgroundColor: getTriageColor(p.triage) }}
                   >
                     {p.triage}
                   </span>
                 </div>
-                <div>{p.waitTime}</div>
-                <div>{p.injury}</div>
+                <div className="al-col-time">{p.waitTime}</div>
+                <div className="al-col-injury">{p.injury}</div>
               </div>
             ))}
           </div>
--- src/components/patientlist/PatientList.js
+++ src/components/patientlist/PatientList.js
@@ -2,33 +2,11 @@ import React from 'react';
 import './PatientList.css';
 
 const patients = [
-    { id: '04', name: 'Justin Chi Cheng sye', triage: 5, time: '15:47' },
-    { id: '02', name: 'Bryant Effendi', triage: 4, time: '03:47' },
-    { id: '06', name: 'Joshua Darren Chandra', triage: 3, time: '01:47' },
-    { id: '08', name: 'Kayden Ciazaria', triage: 2, time: '01:47' },
-    { id: '03', name: 'Troy Devdan', triage: 1, time: '01:47' },
-    { id: '04', name: 'Justin Chi Cheng sye', triage: 5, time: '15:47' },
-    { id: '02', name: 'Bryant Effendi', triage: 4, time: '03:47' },
-    { id: '06', name: 'Joshua Darren Chandra', triage: 3, time: '01:47' },
-    { id: '04', name: 'Justin Chi Cheng sye', triage: 5, time: '15:47' },
-    { id: '02', name: 'Bryant Effendi', triage: 4, time: '03:47' },
-    { id: '06', name: 'Joshua Darren Chandra', triage: 3, time: '01:47' },
-    { id: '08', name: 'Kayden Ciazaria', triage: 2, time: '01:47' },
-    { id: '03', name: 'Troy Devdan', triage: 1, time: '01:47' },
-    { id: '04', name: 'Justin Chi Cheng sye', triage: 5, time: '15:47' },
-    { id: '02', name: 'Bryant Effendi', triage: 4, time: '03:47' },
-    { id: '06', name: 'Joshua Darren Chandra', triage: 3, time: '01:47' },
-    { id: '08', name: 'Kayden Ciazaria', triage: 2, time: '01:47' },
-    { id: '03', name: 'Troy Devdan', triage: 1, time: '01:47' },
-    { id: '04', name: 'Justin Chi Cheng sye', triage: 5, time: '15:47' },
-    { id: '02', name: 'Bryant Effendi', triage: 4, time: '03:47' },
-    { id: '06', name: 'Joshua Darren Chandra', triage: 3, time: '01:47' },
-    { id: '08', name: 'Kayden Ciazaria', triage: 2, time: '01:47' },
-    { id: '03', name: 'Troy Devdan', triage: 1, time: '01:47' },
-    { id: '02', name: 'Bryant Effendi', triage: 4, time: '03:47' },
-    { id: '06', name: 'Joshua Darren Chandra', triage: 3, time: '01:47' },
-    { id: '08', name: 'Kayden Ciazaria', triage: 2, time: '01:47' },
-    { id: '03', name: 'Troy Devdan', triage: 1, time: '01:47' },
+  { id: '04', name: 'Justin', triage: 5, injury: 'Fractured arm', time: '15:47' },
+  { id: '02', name: 'Bryant', triage: 4, injury: 'Head trauma', time: '03:47' },
+  { id: '06', name: 'Joshua', triage: 3, injury: 'Sprained ankle', time: '01:47' },
+  { id: '08', name: 'Kayden', triage: 2, injury: 'Minor burns', time: '01:47' },
+  { id: '03', name: 'Troy', triage: 1, injury: 'Chest pain', time: '01:47' },
 ];
 
 const getTriageColor = (level) => {
@@ -38,42 +16,44 @@ const getTriageColor = (level) => {
 
 const PatientList = () => {
   return (
-    <div className="patient-list-container">
-      <div className="patient-list">
-        {/* Box 1: Title Header */}
-        <div className="list-header">
+    <div className="pl-container">
+      <div className="pl-list">
+        {/* Title Header */}
+        <div className="pl-header">
           <h2>Patient Waiting List</h2>
-          <button className="add-button">+ Add Patient</button>
+          <button className="pl-add-btn">+ Add Patient</button>
         </div>
 
-        {/* Box 2: Column Headers */}
-        <div className="column-header">
-          <div>P.ID</div>
-          <div>Name</div>
-          <div>Triage</div>
-          <div>Time Elapsed</div>
-          <div>Edit</div>
+        {/* Column Headers */}
+        <div className="pl-col-header">
+          <div className="pl-col-pid">P.ID</div>
+          <div className="pl-col-name">Name</div>
+          <div className="pl-col-triage">Triage</div>
+          <div className="pl-col-injury">Injury Type</div>
+          <div className="pl-col-time">Time Elapsed</div>
+          <div className="pl-col-edit">Edit</div>
         </div>
 
-        {/* Box 3: Scrollable Rows */}
-        <div className="patient-rows-container">
-          <div className="patient-rows-scrollable">
+        {/* Patient Rows */}
+        <div className="pl-rows-container">
+          <div className="pl-rows-scrollable">
             {patients.map((p) => (
-              <div className="patient-row" key={p.id}>
-                <div>{p.id}</div>
-                <div>{p.name}</div>
-                <div>
+              <div className="pl-row" key={p.id}>
+                <div className="pl-col-pid">{p.id}</div>
+                <div className="pl-col-name">{p.name}</div>
+                <div className="pl-col-triage">
                   <span
-                    className="triage-circle"
+                    className="pl-triage-circle"
                     style={{ backgroundColor: getTriageColor(p.triage) }}
                   >
                     {p.triage}
                   </span>
                 </div>
-                <div>{p.time}</div>
-                <div>
-                  <button className="icon-button">✎</button>
-                  <button className="admit-button">Admit</button>
+                <div className="pl-col-injury">{p.injury}</div>
+                <div className="pl-col-time">{p.time}</div>
+                <div className="pl-col-edit">
+                  <button className="pl-icon-btn">✎</button>
+                  <button className="pl-admit-btn">Admit</button>
                 </div>
               </div>
             ))}
